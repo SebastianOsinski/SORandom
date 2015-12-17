898edfb92fca47b88efddadb344dca1e6e26889a
Sebastian Osiński <seb.osinski@gmail.com>
2015-12-17 19:12:04 +0100

    Add licenses

--- SORandom/SORandom.swift
+++ SORandom/SORandom.swift
@@ -1,4 +1,31 @@
-///  Collection of psuedorandom generators from various statistical distributions.
+//
+//  SORandom.swift
+//  SORandom
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015 Sebastian Osiński
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+//  Collection of psuedorandom generators from various statistical distributions.
 
 
 // MARK: - Binomial distribution
@@ -10,9 +37,9 @@ Performs single Bernoulli trial with given probability of success.
     
 - returns: Success or failure as Int: 1 or 0.
 */
-    
+
 public func randBinomial(probabilityOfSuccess: Double) -> Int {
-    return Int(randContUniform(0.0, 1.0) < probabilityOfSuccess)
+    return randContUniform(0.0, 1.0) < probabilityOfSuccess ? 1 : 0
 }
 
 /**
@@ -25,7 +52,7 @@ Performs series of independent Bernoulli trials with given probability of succes
 */
 
 public func randBinomials(probabilityOfSuccess: Double, _ sampleLength: Int) -> [Int] {
-    return randContUniforms(0, 1, sampleLength).map { Int($0 < probabilityOfSuccess) }
+    return randContUniforms(0, 1, sampleLength).map { $0 < probabilityOfSuccess ? 1 : 0 }
 }
 
 /**
